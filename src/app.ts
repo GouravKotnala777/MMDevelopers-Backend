c62b606fc1ee9d5d884a4b519bb96dc2155a3698
gouravkotnala <gouravkotnala777@gmail.com>
2024-03-09 21:19:59 +0530

    added_cors_url

--- src/app.ts
+++ src/app.ts
@@ -30,11 +30,11 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+    res.setHeader('Access-Control-Allow-Origin', 'https://mm-developers-frontend.vercel.app');    
     res.setHeader('Access-Control-Allow-Credentials', "true");  
     next();  
 });
-
 
 
 
